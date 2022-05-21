e93294a9bf29ef0089a03b9a6efb08fe5ec25313
Bruno Jaques Olivera de Vargas <70863493+bjvargas@users.noreply.github.com>
2022-05-20 23:42:59 -0300

    Adjustment DTO List

--- src/main/java/ced/characters/management/repository/CharactersRepository.java
+++ src/main/java/ced/characters/management/repository/CharactersRepository.java
@@ -1,7 +1,6 @@
 package ced.characters.management.repository;
 
 import ced.characters.management.models.CharacterSheet;
-import ced.characters.management.vo.CharactersSheetDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
