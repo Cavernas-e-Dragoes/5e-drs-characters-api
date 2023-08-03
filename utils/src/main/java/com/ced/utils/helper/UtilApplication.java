e4fb78891d7516871d88d32eac125c71a0f7a70f
Bruno Vargas <bruno_jaques@hotmail.com>
2023-08-03 13:22:28 -0300

    Adding main to the utils

---
+++ utils/src/main/java/com/ced/utils/helper/UtilApplication.java
@@ -0,0 +1,14 @@
+package com.ced.utils.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilApplication.class, args);
+	}
+
+
+}
