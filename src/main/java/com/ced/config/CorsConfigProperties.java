6182000ea2aac49a5cebe894bc7375a26ef8f6a7
Vargas <bruno_jaques@hotmail.com>
2024-12-01 19:56:58 -0300

    Rename JWTConfig to WebConfig and update CORS.

---
+++ src/main/java/com/ced/config/CorsConfigProperties.java
@@ -0,0 +1,19 @@
+package com.ced.config;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfigProperties {
+
+    private List<String> allowedOrigins;
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
---
+++ src/main/java/com/ced/security/WebConfig.java
@@ -0,0 +1,81 @@
+package com.ced.security;
+
+import com.ced.config.CorsConfigProperties;
+import com.ced.config.SecurityProperties;
+import com.ced.repository.UserRepository;
+import com.ced.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    private final AuthenticationConfiguration configuration;
+
+    @Autowired
+    private CorsConfigProperties corsConfigProperties;
+
+    @Autowired
+    public WebConfig(AuthenticationConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .addFilter(new JWTAuthFilter(configuration.getAuthenticationManager(), userRepository, jwtHelper))
+                .addFilter(new JWTValidFilter(configuration.getAuthenticationManager(), securityProperties))
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsConfigProperties.getAllowedOrigins());
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
